6ec10e0139f73c20391a39495b3ec1ba5d818990
Glitch (slash-sherbet) <none>
2019-09-03 19:28:47 +0000

    🎱🌴 Updated with Glitch

--- events/guildMemberAdd.js
+++
@@ -1,5 +0,0 @@
-module.exports = member => {
-    let username = member.user.username;
-    member.sendMessage('Selamlar,Değerli **' + username + '** Discord Sunucuma Hoşgeldin :kissing_heart: #kurallar a bakmayı unutma,lazım olacağından eminim.');
-
-};
